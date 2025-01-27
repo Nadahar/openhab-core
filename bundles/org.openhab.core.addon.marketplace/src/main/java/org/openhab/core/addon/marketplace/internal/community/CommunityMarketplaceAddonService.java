19d44b5330c1a0adb4792821062616571dd29434
Nadahar <Nadahar@users.noreply.github.com>
2025-01-27 02:45:04 +0100

    Fixes

--- bundles/org.openhab.core.addon.marketplace/src/main/java/org/openhab/core/addon/marketplace/internal/community/CommunityMarketplaceAddonService.java
+++ bundles/org.openhab.core.addon.marketplace/src/main/java/org/openhab/core/addon/marketplace/internal/community/CommunityMarketplaceAddonService.java
@@ -725,10 +725,11 @@ public class CommunityMarketplaceAddonService extends AbstractRemoteAddonService
                 builder.withVersion(latestStable.getVersion());
             }
             if (!latestStable.getCountries().isEmpty()) {
-                if (builder.getCountries() == null) {
+                List<String> builderCountries = builder.getCountries();
+                if (builderCountries == null) {
                     builder.withCountries(latestStable.getCountries());
                 } else {
-                    List<String> c = new ArrayList<>(builder.getCountries());
+                    List<String> c = new ArrayList<>(builderCountries);
                     c.addAll(latestStable.getCountries());
                     builder.withCountries(c);
                 }
@@ -744,10 +745,11 @@ public class CommunityMarketplaceAddonService extends AbstractRemoteAddonService
                 builder.withKeywords(s);
             }
             if (!latestStable.getLoggerPackages().isEmpty()) {
-                if (builder.getLoggerPackages() == null) {
+                List<String> builderLoggerPackages = builder.getLoggerPackages();
+                if (builderLoggerPackages == null) {
                     builder.withLoggerPackages(latestStable.getLoggerPackages());
                 } else {
-                    List<String> l = new ArrayList<>(builder.getLoggerPackages());
+                    List<String> l = new ArrayList<>(builderLoggerPackages);
                     l.addAll(latestStable.getLoggerPackages());
                     builder.withLoggerPackages(l);
                 }
@@ -755,8 +757,8 @@ public class CommunityMarketplaceAddonService extends AbstractRemoteAddonService
             if ((s = latestStable.getMaturity()) != null && !s.isBlank()) {
                 builder.withMaturity(s);
             }
-            Set<String> deps;
             if (!latestStable.getDependsOn().isEmpty()) {
+                Set<String> deps;
                 if ((deps = builder.getDependsOn()) != null) {
                     builder.withDependsOn(Stream.concat(deps.stream(), latestStable.getDependsOn().stream()).collect(Collectors.toSet()));
                 } else {
