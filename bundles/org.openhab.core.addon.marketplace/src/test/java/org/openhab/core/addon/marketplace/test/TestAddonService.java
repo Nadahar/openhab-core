7d4ea1f0632f223349ebed4a5db56d50f654d2b8
Nadahar <Nadahar@users.noreply.github.com>
2025-02-08 00:11:28 +0100

    Slightly redesigned RemoteAddonServvice concurrency logic

--- bundles/org.openhab.core.addon.marketplace/src/test/java/org/openhab/core/addon/marketplace/test/TestAddonService.java
+++ bundles/org.openhab.core.addon.marketplace/src/test/java/org/openhab/core/addon/marketplace/test/TestAddonService.java
@@ -61,12 +61,12 @@ public class TestAddonService extends AbstractRemoteAddonService {
     }
 
     @Override
-    public synchronized void addAddonHandler(MarketplaceAddonHandler handler) {
+    public void addAddonHandler(MarketplaceAddonHandler handler) {
         this.addonHandlers.add(handler);
     }
 
     @Override
-    public synchronized void removeAddonHandler(MarketplaceAddonHandler handler) {
+    public void removeAddonHandler(MarketplaceAddonHandler handler) {
         this.addonHandlers.remove(handler);
     }
 
@@ -123,15 +123,13 @@ public class TestAddonService extends AbstractRemoteAddonService {
         Addon addon = Addon.create(SERVICE_PID + ":" + id).withType("binding").withId(id.substring("binding-".length()))
                 .withVersion(Version.valueOf("4.1.0")).withContentType(TestAddonHandler.TEST_ADDON_CONTENT_TYPE).build();
 
-        synchronized (this) {
-            addonHandlers.forEach(addonHandler -> {
-                try {
-                    addonHandler.install(addon);
-                } catch (MarketplaceHandlerException e) {
-                    // ignore
-                }
-            });
-        }
+        addonHandlers.forEach(addonHandler -> {
+            try {
+                addonHandler.install(addon);
+            } catch (MarketplaceHandlerException e) {
+                // ignore
+            }
+        });
     }
 
     /**
