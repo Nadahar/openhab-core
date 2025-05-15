0bcda28384b288f6c448deffa43ca805ba2c1193
Ravi Nadahar <nadahar@rediffmail.com>
2025-05-15 14:36:25 +0200

    Create ModuleTypeAliases
    Module type aliases let users specify less cryptic, shorter "codes" for module types in the new YAML based format. The full "codes" can still be used.
    
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>

--- bundles/org.openhab.core.model.yaml/src/main/java/org/openhab/core/model/yaml/internal/rules/YamlModuleDTO.java
+++ bundles/org.openhab.core.model.yaml/src/main/java/org/openhab/core/model/yaml/internal/rules/YamlModuleDTO.java
@@ -42,7 +42,7 @@ public class YamlModuleDTO {
         this.id = module.getId();
         this.label = module.getLabel();
         this.description = module.getDescription();
-        this.config = module.getConfiguration().getProperties();
+        this.type = ModuleTypeAliases.typeToAlias(module.getClass(), module.getTypeUID());
         this.type = module.getTypeUID();
     }
 
