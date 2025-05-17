32d3bcb7fec5331af8e0ce8349266830f6edb056
Ravi Nadahar <nadahar@rediffmail.com>
2025-05-17 16:19:31 +0200

    MIMETypeAliases 2

--- bundles/org.openhab.core.model.yaml/src/main/java/org/openhab/core/model/yaml/internal/rules/MIMETypeAliases.java
+++ bundles/org.openhab.core.model.yaml/src/main/java/org/openhab/core/model/yaml/internal/rules/MIMETypeAliases.java
@@ -2,6 +2,7 @@ package org.openhab.core.model.yaml.internal.rules;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
@@ -13,20 +14,29 @@ public class MIMETypeAliases { // TODO: (Nad) Heeader + JavaDocs
     private static final Map<String, String> MIME_TYPE_IDX;
 
     static {
+        /*
+         * The table below is formatted as follows:
+         * - The first column is the MIME type
+         * - The second column is the "primary code" that will be used when translating to alias
+         * - The remaining columns are optional aliases that will only work when translation to MIME type
+         */
         String[][] table = { //
-            {"DSL", "application/vnd.openhab.dsl.rule"}, //
-            {"GROOVY", "application/x-groovy"}, //
-            {"JS", "application/javascript"}, // "application/javascript", "application/ecmascript", "text/javascript", "text/ecmascript" "application/javascript;version=ECMAScript-2021"
-            {"PY", "application/python"}, // "text/python", "application/python", "text/x-python", "application/x-python"
-            {"RUBY", "application/x-ruby"}, //
-            {"NASHORNJS", "application/javascript;version=ECMAScript-5.1"}, // "application/javascript;version=ECMAScript-5.1", "application/ecmascript;version=ECMAScript-5.1", "text/javascript;version=ECMAScript-5.1", "text/ecmascript;version=ECMAScript-5.1"
+            {"application/vnd.openhab.dsl.rule", "RuleDSL", "DSL"}, //
+            {"application/x-groovy", "Groovy"}, //
+            {"application/javascript", "JavaScript", "JS", "GraalJS"}, //
+            {"application/python", "Python", "Python3", "PY", "PY3"}, //
+            {"application/x-python2", "Jython", "JythonPY", "PY2"},
+            {"application/x-ruby", "Ruby", "RB"}, //
+            {"application/javascript;version=ECMAScript-5.1", "NashornJS", "NashornJavaScript" } //
         };
 
         Map<String, String> aliasIdx = new HashMap<>();
         Map<String, String> mimeTypeIdx = new HashMap<>();
         for (String[] entry : table) {
-            aliasIdx.put(entry[0], entry[1]);
-            mimeTypeIdx.put(entry[1], entry[0]);
+            mimeTypeIdx.put(entry[0], entry[1]);
+            for (int i = 1; i < entry.length; i++) {
+                aliasIdx.put(entry[i].toLowerCase(Locale.ROOT), entry[0]);
+            }
         }
         ALIAS_IDX = Collections.unmodifiableMap(aliasIdx);
         MIME_TYPE_IDX = Collections.unmodifiableMap(mimeTypeIdx);
@@ -39,7 +49,7 @@ public class MIMETypeAliases { // TODO: (Nad) Heeader + JavaDocs
     }
 
     public static String aliasToType(String alias) {
-        return ALIAS_IDX.getOrDefault(alias, alias);
+        return ALIAS_IDX.getOrDefault(alias.toLowerCase(Locale.ROOT), alias);
     }
 
     public static String mimeTypeToAlias(String type) {
