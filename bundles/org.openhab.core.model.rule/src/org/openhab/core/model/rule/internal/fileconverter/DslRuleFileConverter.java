32acf7e49278cc99a7db2258d71df06ea244803d
Ravi Nadahar <nadahar@rediffmail.com>
2026-02-23 22:51:29 +0100

    Fumbling

--- bundles/org.openhab.core.model.rule/src/org/openhab/core/model/rule/internal/fileconverter/DslRuleFileConverter.java
+++ bundles/org.openhab.core.model.rule/src/org/openhab/core/model/rule/internal/fileconverter/DslRuleFileConverter.java
@@ -17,8 +17,6 @@ import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.core.automation.Rule;
 import org.openhab.core.automation.fileconverter.RuleParser;
 import org.openhab.core.automation.fileconverter.RuleSerializer;
-import org.openhab.core.config.core.ConfigDescriptionRegistry;
-import org.openhab.core.i18n.LocaleProvider;
 import org.openhab.core.model.core.ModelRepository;
 import org.openhab.core.model.rule.rules.RuleModel;
 import org.openhab.core.model.rule.rules.RulesFactory;
@@ -37,18 +35,18 @@ public class DslRuleFileConverter implements RuleSerializer, RuleParser {
     private final ModelRepository modelRepository;
 //    private final DSLRuleProvider ruleProvider;
 //    private final GenericItemChannelLinkProvider itemChannelLinkProvider;
-    private final LocaleProvider localeProvider;
+//    private final LocaleProvider localeProvider;
 
     private final Map<String, RuleModel> elementsToGenerate = new ConcurrentHashMap<>();
 
     @Activate
-    public DslRuleFileConverter(@Reference ModelRepository modelRepository,
+    public DslRuleFileConverter(@Reference ModelRepository modelRepository/*,
             final @Reference ConfigDescriptionRegistry configDescRegistry,
-            final @Reference LocaleProvider localeProvider) {
+            final @Reference LocaleProvider localeProvider*/) {
         this.modelRepository = modelRepository;
 //        this.thingProvider = thingProvider;
 //        this.itemChannelLinkProvider = itemChannelLinkProvider;
-        this.localeProvider = localeProvider;
+//        this.localeProvider = localeProvider;
     }
 
     @Override
